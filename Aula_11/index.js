e1ea26011fb16ac0dd43bcf0e8660d0b4df46736
Leoruiz197 <ruizruizleo@hotmail>
2021-11-08 21:40:46 -0300

    CRUD completo

--- Aula_11/index.js
+++ Aula_11/index.js
@@ -8,15 +8,15 @@
 const express = require("express");  //chamando o express
 const app = express();  //definindo o app como express
 require('dotenv').config(); // config do .env para acessar as VARs de ambiente
-const cors = require("cors");
+const cors = require("cors");  // importando o CORS para nosso APP
 app.use(express.json());  //definindo o JSON no projeto
 
 const Conn = require("./models/conn/index"); //importando a conexao
 
 Conn(); //executa a func de conexao
 
-app.use(cors());
-app.options("*", cors());
+app.use(cors()); // usando o CORS no APP
+app.options("*", cors()); // Configurando o CORS para liberar todo trafego.
 
 app.get('/', (req,res) => {
     res.status(200).json({message:"API Rick and Morty ok"});
--- Aula_11/routers/personagens.routes.js
+++ Aula_11/routers/personagens.routes.js
@@ -15,6 +15,16 @@ router.get("/readAll", async (req,res) => {
     });
 });
 
+router.get("/readSingle/:id", async (req,res) => {
+    const id  = req.params.id;
+    await Personagem.findById(id).then((personagem) => {
+        res.status(200).json(personagem);
+    }).catch((err) => {
+        res.status(404).json({message: "nenhum personagem foi encontrado"});
+        console.error(err);
+    });
+});
+
 router.post("/create", async (req,res) => {
     await Personagem.create(req.body).then( () => {
         res.status(201).json({message: "Personagem inserido com sucesso!!!"})
@@ -22,7 +32,27 @@ router.post("/create", async (req,res) => {
         res.status(400).json({message: "algo esta errado"});
         console.error(err);
     });
-
 });
+
+router.put("/update/:id", async (req,res) => {
+    const id  = req.params.id;
+    await Personagem.findByIdAndUpdate(id,req.body).then(() => {
+        res.status(200).json({message: "Personagem atualizado com sucesso!!!"})
+    }).catch((err) => {
+        res.status(400).json({message: "Algo deu errado!!!"});
+        console.error(err);
+    });
+});
+
+router.delete("/delete/:id", async (req,res) => {
+    const id  = req.params.id;
+
+    await Personagem.findByIdAndDelete(id).then(() => {
+        res.status(200).json({message: "Personagem deletado com sucesso!!!"});
+    }).catch((err) => {
+        res.status(404).json({message: "nenhum personagem foi encontrado"});
+        console.error(err);
+    });
+})
 
 module.exports = router;
